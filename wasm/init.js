bd20043dcccb5caec30dc80dc1f5a7b50a5999b8
natemoo-re <nate.moore.27@gmail.com>
2021-07-30 05:20:24 -0400

    1 file modified

--- wasm/init.js
+++ wasm/init.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
+const path = require('path');
 require('./exec');
 
 async function init() {
-    const go = new Go();
-    const buf = fs.readFileSync('./tycho.wasm');
-    await WebAssembly.instantiate(new Uint8Array(buf), go.importObject);
-    return { BuildDocument: globalThis.BuildDocument };
+  const go = new Go();
+  console.log(path.resolve('./tycho.wasm'));
+  const buf = fs.readFileSync(path.resolve('./tycho.wasm'));
+  await WebAssembly.instantiate(new Uint8Array(buf), go.importObject);
+  return { BuildDocument: globalThis.BuildDocument };
 }
 
 module.exports = init;
